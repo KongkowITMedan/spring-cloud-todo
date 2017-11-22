d151a5219ac7ae3a6e55ce13f1e6c2753d767ccd
Maikel Chandika <mkdika@gmail.com>
2017-11-22 12:51:16 +0700

    fix unit test dependencies error

--- todoservice/src/test/java/com/mkdika/cloudtodo/todoservice/TodoserviceApplicationTests.java
+++ todoservice/src/test/java/com/mkdika/cloudtodo/todoservice/TodoserviceApplicationTests.java
@@ -3,14 +3,16 @@ package com.mkdika.cloudtodo.todoservice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class TodoserviceApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
