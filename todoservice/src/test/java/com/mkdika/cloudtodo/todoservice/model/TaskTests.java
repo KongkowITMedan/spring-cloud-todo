c187dce91714e1cf410cdc134c6f0d65a20502bd
Maikel Chandika <mkdika@gmail.com>
2017-11-24 09:02:30 +0700

    update fix covtest for todoservice

--- todoservice/src/test/java/com/mkdika/cloudtodo/todoservice/model/TaskTests.java
+++ todoservice/src/test/java/com/mkdika/cloudtodo/todoservice/model/TaskTests.java
@@ -8,27 +8,40 @@ import org.junit.Test;
  * @author Maikel Chandika <mkdika@gmail.com>
  */
 public class TaskTests {
-    
+
     @Test
-    public void newInstanceNoArgConstructor() {
+    public void newInstanceNoArgConst() {
         Assert.assertNotNull(new Task());
     }
-    
+
     @Test
-    public void newInstanceAllArgConstructor() {
-        Task task = new Task(6, "Kerja Keras", false,true);
-        Assert.assertEquals("Kerja Keras",task.getContent());
+    public void newInstanceArgConst() {
+        Task task = new Task(6, "Kerja Keras", false, true);
+        Assert.assertEquals("Kerja Keras", task.getContent());
     }
-    
+
+    /*
+        Unit Test for Lombok @Data annotation to be coveraged all.
+        @Data is consist of :
+        - @Setter
+        - @Getter
+        - @ToString
+        - @EqualsAndHashCode                
+     */
     @Test
-    public void testAssignValue() {
+    public void testDataEntity() {
         Task task = new Task();
         task.setId(7);
         task.setContent("Testing Kerja");
         task.setIsComplete(true);
         task.setIsEditable(false);
+        Assert.assertNotNull(task.getId());
+        Assert.assertEquals("Testing Kerja", task.getContent());
         Assert.assertFalse(task.getIsEditable());
         Assert.assertTrue(task.getIsComplete());
-        Assert.assertNotNull(task.getId());
+        
+        Assert.assertNotNull(task.toString());
+        Assert.assertNotNull(task.hashCode());
+        Assert.assertFalse(task.equals(new Task()));        
     }
 }
