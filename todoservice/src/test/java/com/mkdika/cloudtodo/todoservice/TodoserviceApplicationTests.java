e4fa5fdb962258422575ccec4223cd924e407b88
Maikel Chandika <mkdika@gmail.com>
2017-11-24 07:35:24 +0700

    fixing gateway server

--- todoservice/src/test/java/com/mkdika/cloudtodo/todoservice/TodoserviceApplicationTests.java
+++ todoservice/src/test/java/com/mkdika/cloudtodo/todoservice/TodoserviceApplicationTests.java
@@ -4,16 +4,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = TodoserviceApplication.class)
-@WebAppConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @ActiveProfiles("test")
 public class TodoserviceApplicationTests {
-
     @Test
     public void contextLoads() {
-    }    
+    }        
 }
+
