f27965076d97c58c0ddb6e847c16da25ccc4880f
Maikel Chandika <mkdika@gmail.com>
2018-01-03 09:28:19 +0700

    fix broken build

--- msgservice/src/test/java/com/mkdika/cloudtodo/msgservice/MsgserviceApplicationTests.java
+++ msgservice/src/test/java/com/mkdika/cloudtodo/msgservice/MsgserviceApplicationTests.java
@@ -2,15 +2,14 @@ package com.mkdika.cloudtodo.msgservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class MsgserviceApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void applicationStarts() {
+        MsgserviceApplication.main(new String[]{"--spring.config.location=classpath:bootstrap-test.properties"});
+    }
 }
