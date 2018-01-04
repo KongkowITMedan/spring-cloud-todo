28165bfdcba9f1e99b353f1047e10ab07fc985bb
Maikel Chandika <mkdika@gmail.com>
2018-01-04 13:31:02 +0700

    fix cors zuul

--- gatewayservice/src/main/java/com/mkdika/cloudtodo/gatewayservice/GatewayserviceApplication.java
+++ gatewayservice/src/main/java/com/mkdika/cloudtodo/gatewayservice/GatewayserviceApplication.java
@@ -22,17 +22,6 @@ public class GatewayserviceApplication {
     public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowCredentials(false);
-//        config.setMaxAge(3600L);        
-//        config.addAllowedOrigin("http://localhost:8080");
-//        config.addAllowedHeader("header1,header2,header3");
-//        config.addExposedHeader("header1,header2");
-//        config.addAllowedMethod("OPTIONS");        
-//        config.addAllowedMethod("GET");
-//        config.addAllowedMethod("PUT");
-//        config.addAllowedMethod("POST");
-//        config.addAllowedMethod("DELETE");        
-//source.registerCorsConfiguration("/**", config);
         
         config.setAllowCredentials(true);
         config.addAllowedOrigin("*");
