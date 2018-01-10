772541484553ab713ca4f6ed474bb9c5267f943d
Des Dulianto <desdulianto@gmail.com>
2018-01-10 15:29:00 +0700

    display audit trail message box

--- vue-todo/src/components/Card.vue
+++ vue-todo/src/components/Card.vue
@@ -1,34 +1,47 @@
 <template>
-  <div class="card">
-    <div class="card-content" :class="{ 'no-padding': task.editable }">
-      <textarea v-if="task.editable" class="textarea" v-model="content"></textarea>
-      <p v-else class="subtitle">{{ task.content }}</p>
-    </div>
+  <div>
+    <div class="card">
+      <div class="card-content" :class="{ 'no-padding': task.editable }">
+        <textarea v-if="task.editable" class="textarea" v-model="content"></textarea>
+        <p v-else class="subtitle">{{ task.content }}</p>
+      </div>
 
-    <footer class="card-footer">
-      <template v-if="task.editable">
-        <a @click="updateCard" class="card-footer-item">Save</a>
-      </template>
-      <template v-else-if="task.complete">
-        <a @click="redoTask" class="card-footer-item">Redo</a>
-      </template>
-      <template v-else>
-        <a @click="editCard" class="card-footer-item">Edit</a>
-        <a @click="clearTask" class="card-footer-item">Done</a>
-      </template>
-    </footer>
+      <footer class="card-footer">
+        <template v-if="task.editable">
+          <a @click="updateCard" class="card-footer-item">Save</a>
+        </template>
+        <template v-else-if="task.complete">
+          <a @click="redoTask" class="card-footer-item">Redo</a>
+        </template>
+        <template v-else>
+          <a @click="editCard" class="card-footer-item">Edit</a>
+          <a @click="clearTask" class="card-footer-item">Done</a>
+          <a @click="taskTrail" class="card-footer-item">Trail</a>
+        </template>
+      </footer>
+    </div>
+    <MessageBox :title="title" :message="message" ref="messageBox" />
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import MessageBox from './MessageBox.vue'
+
 export default {
   name: 'card',
+
+  components: {
+    MessageBox
+  },
 
   props: ['task'],
 
   data () {
     return {
-      content: ''
+      content: '',
+      title: '',
+      message: ''
     }
   },
 
@@ -55,8 +68,53 @@ export default {
       var task = Object.assign({}, this.task)
       task.complete = false
       this.$store.dispatch('task/saveTask', task)
+    },
+
+    loadTaskTrail (context, task) {
+      try {
+        var href = task.links.filter(x => x.rel === 'trail')[0].href
+        axios.get(href)
+          .then(res => {
+            context.commit('displayMessageBox', {task: task, trails: res.data})
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    showMessage (title, message) {
+      this.title = title
+      this.message = message
+
+      this.$refs.messageBox.show()
+    },
+
+    taskTrail () {
+      var task = this.task
+      try {
+        var href = task.links.filter(x => x.rel === 'trail')[0].href
+        axios.get(href)
+          .then(res => {
+            var message = '<table>' +
+                          '<tr><th>Change Time</th><th>Message</th></tr>' +
+                          res.data.map(x => '<tr>' +
+                            '<td>' + x.changeTime + '</td>' +
+                            '<td>' + x.message + '</td>' + 
+                            '</tr>').join('') +
+                          '</table>'
+            this.showMessage(task.content, message)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+      } catch (e) {
+        console.log(e)
+      }
     }
-  }
+  },
 }
 </script>
 
---
+++ vue-todo/src/components/MessageBox.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="modal" :class="{'is-active': active}">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">{{title}}</p>
+          <button class="delete" aria-label="close" @click="close"></button>
+        </header>
+        <section class="modal-card-body" v-html="message">
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button is-success" @click="close">Close</button>
+        </footer>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'messagebox',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  data () {
+    return {
+      active: false
+    }
+  },
+  methods: {
+    show () {
+      this.active = true
+    },
+    close () {
+      this.active = false
+    }
+  }
+}
+</script>
